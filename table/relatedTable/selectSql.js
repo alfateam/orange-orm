edffc164a2fff4619e654ff30db21904eac04ec2
Lars-Erik Roald <lars.roald@gmail.com>
2014-03-14 08:54:57 +0100

    selectSql

--- table/relatedTable/selectSql.js
+++ table/relatedTable/selectSql.js
@@ -1,1 +1,11 @@
-//todo
+var newParameterized = require('../query/newParameterized');
+var newBoolean = require('../column/newBoolean');
+
+function newSelectSql(table, alias) {
+	var colName = table._primaryColumns[0]._dbName;
+	var sql = 'SELECT ' + alias + '.' + colName + ' FROM ' + table._dbName + ' AS ' + alias;
+	sql = newParameterized(sql);
+	return newBoolean(sql);
+}
+
+module.exports = newSelectSql;
