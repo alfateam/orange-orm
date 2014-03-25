2f5c09ebca16f70a461ec317d77126455f036e67
Lars-Erik Roald <lars.roald@gmail.com>
2014-03-25 08:54:03 +0100

    missing semicolons

--- tableSpec/integrationSpec/defineTable/andOr.js
+++ tableSpec/integrationSpec/defineTable/andOr.js
@@ -1,8 +1,8 @@
 function act(c) {
 	c.expected = "select _0.oOrderId,_0.oCustomerId from order _0 where _0.oOrderId=2 AND (_0.oOrderId=3 OR _0.oOrderId=4) AND _0.discriminatorColumn='foo' AND _0.discriminatorColumn2='baz'";
-	var filter = c.orderTable.id.eq(2)
-	var filter2 = c.orderTable.id.eq(3)
-	var filter3 = c.orderTable.id.eq(4)
+	var filter = c.orderTable.id.eq(2);
+	var filter2 = c.orderTable.id.eq(3);
+	var filter3 = c.orderTable.id.eq(4);
 	c.filter = filter.and(filter2.or(filter3));
 	c.newQuery();
 	
