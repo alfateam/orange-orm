f916b9763686056185391928c9177bc460aa5b92
Lars-Erik Roald <lars.roald@gmail.com>
2014-04-15 12:05:45 +0200

    promise handles empty

--- table/promise.js
+++ table/promise.js
@@ -3,10 +3,9 @@ var objectToCallback = require('./objectToCallback');
 
 function newPromise(func) {
 	if (!func)
-		return objectToCallback();
+		return new Promise(objectToCallback());
 	return new Promise(func);
 }
 
 newPromise.all = Promise.all;
-//handle empty
 module.exports = newPromise;
