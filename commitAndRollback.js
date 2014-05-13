a5c73caa965be66c6afe2d06611393f5921b3d9b
Lars-Erik Roald <lars.roald@gmail.com>
2014-05-13 08:56:15 +0200

    deleted redundant files

--- commitAndRollback.js
+++
@@ -1,8 +0,0 @@
-var commit = require('./table/commit');
-var rollback = require('./table/rollback');
-
-function resolveCommitAndRollback(success) {
-	success(commit, rollback);
-};
-
-module.exports = resolveCommitAndRollback;
--- commitAndRollbackSpec/whenReq.js
+++
@@ -1,6 +0,0 @@
-var when = require('a').when;
-var c = {};
-
-when(c)
-	.it('should return commit and rollback').assertDoesNotThrow(c.success.verify)
-	;
