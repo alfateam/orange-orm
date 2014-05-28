9a62c4b0e55026900afbcdb9678b0b2653b078f4
Lars-Erik Roald <lars.roald@gmail.com>
2014-05-28 11:22:04 +0200

    examples moved to rdb-demo repo

--- examples/customer.js
+++
@@ -1,7 +0,0 @@
-var table = require('../index').table;
-
-customer = table('_customer');
-customer.primaryColumn('cId').string().as('id');
-customer.column('cName').string().as('name');
-
-module.exports = customer;
