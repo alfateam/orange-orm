9d51651ffed6b6bce4a8c7bb559a97b07489a762
Lars-Erik Roald <lroal@hpUbuntuLars.(none)>
2014-09-20 20:50:45 +0200

    deleted redundant code and fixed to correct param char

--- tableSpec/integrationSpec/defineTable/containsUnsafe.js
+++ tableSpec/integrationSpec/defineTable/containsUnsafe.js
@@ -1,5 +1,5 @@
 function act(c) {
-	c.expected = "select _0.oOrderId,_0.oCustomerId from order _0 where _0.oCustomerId LIKE '%$%' AND _0.discriminatorColumn='foo' AND _0.discriminatorColumn2='baz'";
+	c.expected = "select _0.oOrderId,_0.oCustomerId from order _0 where _0.oCustomerId LIKE '%?%' AND _0.discriminatorColumn='foo' AND _0.discriminatorColumn2='baz'";
 	c.expectedParam = 'foo_';
 	c.filter = c.orderTable.customerId.contains(c.expectedParam);	
 	c.newQuery();
