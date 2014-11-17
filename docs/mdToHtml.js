783b2eb9d72a6f951556897edd0bee3c7ea47d5c
Lars-Erik Roald <lars.roald@gmail.com>
2014-11-17 09:24:48 +0100

    deleted unused deps

--- docs/mdToHtml.js
+++
@@ -1,6 +0,0 @@
-var marked = require('marked');
-var fs = require('fs');
-
-var str = fs.readFileSync('./docs.md').toString();
-var md = marked(str);
-fs.writeFileSync('./docs.html', md);
--- mdToHtml.js
+++
@@ -1,6 +0,0 @@
-var marked = require('marked');
-var fs = require('fs');
-
-var str = fs.readFileSync('./README.md').toString();
-var md = marked(str);
-fs.writeFileSync('./README.html', md);
