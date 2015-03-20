d4f7542ec3c8ca4a4c4b8f637bfad98aa9fa95d7
Lars-Erik Roald <lars.roald@gmail.com>
2015-03-20 10:27:08 +0100

    like always use parameter

--- tableSpec/integrationSpec/defineTable/startsWith.js
+++ tableSpec/integrationSpec/defineTable/startsWith.js
@@ -1,5 +1,5 @@
 function act(c) {
-	c.expected = "select _0.oOrderId as s_00,_0.oCustomerId as s_01 from order _0 where _0.oCustomerId LIKE 'foo%' AND _0.discriminatorColumn='foo' AND _0.discriminatorColumn2='baz'";
+	c.expected = "select _0.oOrderId as s_00,_0.oCustomerId as s_01 from order _0 where _0.oCustomerId LIKE ? AND _0.discriminatorColumn='foo' AND _0.discriminatorColumn2='baz'";
 	c.filter = c.orderTable.customerId.startsWith('foo');	
 	c.newQuery();
 	
