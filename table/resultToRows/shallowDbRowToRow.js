e94ae0bee63e644d96622180c990ae387bd4941d
Lars-Erik Roald <lars.roald@gmail.com>
2015-04-23 11:45:08 +0200

    Improved performance in shallowDbRowToRow

--- table/resultToRows/shallowDbRowToRow.js
+++ table/resultToRows/shallowDbRowToRow.js
@@ -7,117 +7,127 @@ var newCascadeDeleteStrategy = require('../newCascadeDeleteStrategy');
 var _delete = require('./delete');
 var newObject = require('../../newObject');
 
-function shallowDbRowToRow(table, values) {	
-	var row = {};
-	var columns  = {};
-	var emitChanged = {};
-	var getRelated = {};
-	table._columns.forEach(addColumn);
+function shallowDbRowToRow(table, values) {
+    if (table.shallowDbRowToRow)
+        return table.shallowDbRowToRow(values);
+    defineRow(table);
+    return table.shallowDbRowToRow(values);
+}
 
-	function addColumn(column) {
-		var alias = column.alias;
-		defineColumnProperty(alias);
-		emitChanged[alias] = newEmitEvent();
-		columns[alias] = column;
-	}
+function defineRow(table) {
 
-	function defineColumnProperty(name) {
-		Object.defineProperty(row, name, {
-    		get: function() {        			
-    			return values[name];
-       		},
-       		set : function(value) {
-       			var oldValue = values[name];
-       			var column = columns[name];
-       			value = column.purify(value);
-       			values[name] = value;
-       			updateField(table, columns[name], row);
-       			emitChanged[name](row, column, value, oldValue);       			
-       			}
-    	});
-	}
+    function Row(values) {
+        this._values = values;
+        this._related = {};
+        this._emitChanged = {};
+    }
 
-	setRelated();
+    table._columns.forEach(addColumn);
 
-	function setRelated() {
-		var relations = table._relations;
-		for(var relationName in relations) {			
-			var relation = relations[relationName];
-			setSingleRelated(relationName,relation);
-		}	
-	}
+    function addColumn(column) {
+        defineColumnProperty(column);
+        // this._emitChanged[alias] = newEmitEvent();
+    }
 
-	function setSingleRelated(name, relation) {
-		Object.defineProperty(row, name, {
-    		get: function() {        			
-    				return createGetRelated(name)();
-       		}
-    	});
-	}
+    function defineColumnProperty(column) {
+        var name = column.alias;
+        Object.defineProperty(Row.prototype, name, {
+            get: function() {
+                return this._values[name];
+            },
+            set: function(value) {
+                var oldValue = this._values[name];
+                value = column.purify(value);
+                this._values[name] = value;
+                updateField(table, column, this);
+                // this._emitChanged[name](this, column, value, oldValue);
+            }
+        });
+    }
 
-	function createGetRelated(alias) {
-		var get = getRelated[alias];
-    	if (!get) {
-    		var relation = table._relations[alias];
-    		get = relation.toGetRelated(row);
-    		getRelated[alias] = get;
-    	}
-    	return get;
-	}
+    setRelated();
 
-	row.subscribeChanged = function(onChanged, name) {
-		if (name) {
-			emitChanged[name].add(onChanged);
-			return;
-		}
-		for(name in emitChanged) {
-			emitChanged[name].add(onChanged);
-		}
-	};
+    function setRelated() {
+        var relations = table._relations;
+        for (var relationName in relations) {
+            var relation = relations[relationName];
+            setSingleRelated(relationName, relation);
+        }
+    }
 
-	row.unsubscribeChanged = function(onChanged, name) {
-		if (name) {
-			emitChanged[name].tryRemove(onChanged);
-			return;
-		}
-		for(name in emitChanged) {
-			emitChanged[name].tryRemove(onChanged);
-		}				
-	};
+    function setSingleRelated(name, relation) {
+        Object.defineProperty(Row.prototype, name, {
+            get: function() {
+                return createGetRelated(this, name)();
+            }
+        });
+    }
 
-	row.toJSON = function(strategy) {		
-		return row.toDto.apply(null,arguments).then(JSON.stringify);
-	};
+    function createGetRelated(row, alias) {
+        var get = row._related[alias];
+        if (!get) {
+            var relation = table._relations[alias];
+            get = relation.toGetRelated(row);
+            row._related[alias] = get;
+        }
+        return get;
+    }
 
-	row.toDto = function(strategy) {
-		var args = Array.prototype.slice.call(arguments, 0);
-		args.push(table);			
-		strategy = extractStrategy.apply(null,args);		
-		var toDto = newToDto(strategy, table);
-		return toDto(row);
-	};
+    Row.prototype.subscribeChanged = function(onChanged, name) {
+        // if (name) {
+        //     emitChanged[name].add(onChanged);
+        //     return;
+        // }
+        // for (name in emitChanged) {
+        //     emitChanged[name].add(onChanged);
+        // }
+    };
 
-	row.expand = function(alias) {
-		var get = createGetRelated(alias);
-    	get.expanded = true;
-	};
+    Row.prototype.unsubscribeChanged = function(onChanged, name) {
+        // if (name) {
+        //     emitChanged[name].tryRemove(onChanged);
+        //     return;
+        // }
+        // for (name in emitChanged) {
+        //     emitChanged[name].tryRemove(onChanged);
+        // }
+    };
 
-	row.isExpanded = function(alias) {
-		var get = createGetRelated(alias);
-    	return get.expanded;
-	};
-	
-	row.delete = function(strategy) {
-		strategy = extractDeleteStrategy(strategy, table);
-		_delete(row, strategy, table);
-	};
+    Row.prototype.toJSON = function(strategy) {
+        return this.toDto.apply(null, arguments).then(JSON.stringify);
+    };
 
-	row.cascadeDelete = function() {
-		var strategy = newCascadeDeleteStrategy(newObject(), table);
-		_delete(row, strategy, table);
-	};
+    Row.prototype.toDto = function(strategy) {
+        var args = Array.prototype.slice.call(arguments, 0);
+        args.push(table);
+        strategy = extractStrategy.apply(null, args);
+        var toDto = newToDto(strategy, table);
+        return toDto(this);
+    };
 
-	return row;
+    Row.prototype.expand = function(alias) {
+        var get = createGetRelated(this, alias);
+        get.expanded = true;
+    };
+
+    Row.prototype.isExpanded = function(alias) {
+        var get = createGetRelated(this, alias);
+        return get.expanded;
+    };
+
+    Row.prototype.delete = function(strategy) {
+        strategy = extractDeleteStrategy(strategy, table);
+        _delete(this, strategy, table);
+    };
+
+    Row.prototype.cascadeDelete = function() {
+        var strategy = newCascadeDeleteStrategy(newObject(), table);
+        _delete(this, strategy, table);
+    };
+
+    table.shallowDbRowToRow = function(values) {
+    	return new Row(values);
+    }
 }
 
 module.exports = shallowDbRowToRow;
