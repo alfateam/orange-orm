43b27843df94e44b62315e3e783f4d1b452242ed
Lars-Erik Roald <lars.roald@gmail.com>
2015-06-12 13:43:47 +0200

    dates can accept ISO strings without timezone.

--- table/column/date/tryParseISOSpec/req/whenTry.js
+++ table/column/date/tryParseISOSpec/req/whenTry.js
@@ -2,5 +2,6 @@ var when = require('a').when;
 var c = {};
 
 when(c)
-	.it('should return iso unchanged').assertEqual(c.iso, c.returned)
-	.it('should return iso2 unchanged').assertEqual(c.iso2, c.returned2)
+	.it('should return isoWithJsTimezone unchanged').assertEqual(c.isoWithJsTimezone, c.returned)
+	.it('should return isoWithPgTimezone unchanged').assertEqual(c.isoWithPgTimezone, c.returned2)
+	.it('should return isoWithoutTimezone unchanged').assertEqual(c.isoWithoutTimezone, c.returned3)
