957cd4d6fd550872066cce84a8a54af07a34a9e6
Lars-Erik Roald <lars.roald@gmail.com>
2015-06-24 22:56:24 +0200

    deleted accidental

--- table/joinRelation/newGetRelatedSpec/notExpanded/isDirty/whenGet.js
+++
@@ -1,6 +0,0 @@
-var when = require('a').when;
-var c = {};
-
-when(c)
-    .it('should set expanded').assertEqual(true, c.sut.expanded)
-    .it('should return related row from cache').assertEqual(c.expected, c.returned)
