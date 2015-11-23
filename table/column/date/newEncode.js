e558deaaa8f190efe4414a5b8131b02426e0af5f
Lars-Erik Roald <lars.roald@gmail.com>
2015-11-23 23:05:11 +0100

    another bug in encodeDate

--- table/column/date/newEncode.js
+++ table/column/date/newEncode.js
@@ -4,24 +4,19 @@ var newEncodeSafe = require('../newEncodeSafe');
 var getSessionSingleton = require('../../getSessionSingleton');
 
 function _new(column) {
+    var encode = function(value) {
+        value = purify(value);
+        if (value == null) {
+            if (column.dbNull === null)
+                return newPara('null');
+            return newPara('\'' + column.dbNull + '\'');
+        }
+        var encodeCore = getSessionSingleton('encodeDate');
+        return newPara(encodeCore(value));
+    };
 
-
-	var encode = function(value) {
-		value = purify(value);
-		if (value == null) {
-			if (column.dbNull === null)
-				return newPara('null');
-			return newPara('\'' + column.dbNull + '\'');
-		}
-		if (value.toISOString) {
-			var encodeCore = getSessionSingleton('encodeDate');
-			return newPara(encodeCore(value));
-		}
-		return newPara("'" + value + "'");
-	};
-
-	encode.safe = newEncodeSafe(column, purify);
-	return encode;
+    encode.safe = newEncodeSafe(column, purify);
+    return encode;
 
 }
 
