b4d6d9e786d29ccdf9964e6964ad7166eae7af10
Lars-Erik Roald <lars.roald@gmail.com>
2016-06-21 10:21:57 +0200

    improved sql generate performance

--- table/commands/newInsertCommand.js
+++ table/commands/newInsertCommand.js
@@ -2,31 +2,58 @@ var newInsertCommandCore = require('./newInsertCommandCore');
 var newImmutable = require('../../newImmutable');
 
 function newInsertCommand(table, row) {
-	var c = {};
-	var _getCoreCommand = newImmutable(newInsertCommandCore);
+    return new InsertCommand(table, row);
+    // var c = {};
+    // var _getCoreCommand = newImmutable(newInsertCommandCore);
 
-	c.sql = function() {
-		return getCoreCommand().sql();
-	};
+    // c.sql = function() {
+    //     return getCoreCommand().sql();
+    // };
 
-	Object.defineProperty(c, 'parameters', {
-        get : function() {
-      			return getCoreCommand().parameters;   	
-      		}	 
-         }
-    );
+    // Object.defineProperty(c, 'parameters', {
+    //     get: function() {
+    //         return getCoreCommand().parameters;
+    //     }
+    // });
 
-    function getCoreCommand() {
-    	return _getCoreCommand(table, row);
-    }
+    // function getCoreCommand() {
+    //     return _getCoreCommand(table, row);
+    // }
 
-    c.endEdit = c.sql;
-    
-    c.matches = function(otherRow) {
-      return row == otherRow;
-    };
+    // c.endEdit = c.sql;
 
-	return c;
+    // c.matches = function(otherRow) {
+    //     return row == otherRow;
+    // };
+
+    // return c;
 }
+
+
+function InsertCommand(table, row) {
+    this.__getCoreCommand = newImmutable(newInsertCommandCore);
+    this._table = table;
+    this._row = row;
+}
+
+InsertCommand.prototype._getCoreCommand = function() {
+    return this.__getCoreCommand(this._table, this._row);
+};
+
+InsertCommand.prototype.sql = function() {
+    return this._getCoreCommand().sql();
+};
+
+InsertCommand.prototype.matches = function(otherRow) {
+    return this._row == otherRow;
+};
+
+InsertCommand.prototype.endEdit = InsertCommand.prototype.sql;
+
+Object.defineProperty(InsertCommand.prototype, 'parameters', {
+    get: function() {
+        return this._getCoreCommand().parameters;
+    }
+});
 
 module.exports = newInsertCommand;
