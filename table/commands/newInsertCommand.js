3dc941aa32eae462836d5a7b0da87067d811fd7e
Lars-Erik Roald <lars.roald@gmail.com>
2016-06-21 10:38:57 +0200

    removed commented code

--- table/commands/newInsertCommand.js
+++ table/commands/newInsertCommand.js
@@ -3,32 +3,7 @@ var newImmutable = require('../../newImmutable');
 
 function newInsertCommand(table, row) {
     return new InsertCommand(table, row);
-    // var c = {};
-    // var _getCoreCommand = newImmutable(newInsertCommandCore);
-
-    // c.sql = function() {
-    //     return getCoreCommand().sql();
-    // };
-
-    // Object.defineProperty(c, 'parameters', {
-    //     get: function() {
-    //         return getCoreCommand().parameters;
-    //     }
-    // });
-
-    // function getCoreCommand() {
-    //     return _getCoreCommand(table, row);
-    // }
-
-    // c.endEdit = c.sql;
-
-    // c.matches = function(otherRow) {
-    //     return row == otherRow;
-    // };
-
-    // return c;
 }
-
 
 function InsertCommand(table, row) {
     this.__getCoreCommand = newImmutable(newInsertCommandCore);
