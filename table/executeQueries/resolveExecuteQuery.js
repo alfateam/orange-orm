10deb7b611f59df00cb2f7f077964cf8bf4fc137
Lars-Erik Roald <lars.roald@gmail.com>
2017-02-13 21:53:19 +0100

    handle multiple queries in mySql

--- table/executeQueries/resolveExecuteQuery.js
+++ table/executeQueries/resolveExecuteQuery.js
@@ -1,25 +1,28 @@
 var getSessionSingleton = require('../getSessionSingleton');
 
 function resolveExecuteQuery(query) {
-	return resolve;
-	
-	function resolve(success,failed) {
-		var client = getSessionSingleton('dbClient');
-	
-		client.executeQuery(query, onCompleted);
+    return resolve;
 
-		function onCompleted(err,rows) {
-			if(!err) {
-				Object.defineProperty(rows, 'queryContext', {
-					value: query.queryContext,
-					enumerable: false
-				});
-				success(rows);
-			}
-			else
-				failed(err);
-		}		
-	}
+    function resolve(success, failed) {
+        var client = getSessionSingleton('dbClient');
+
+        client.executeQuery(query, onCompleted);
+
+        function onCompleted(err, rows) {
+            if (!err) {
+                var lastIndex = rows.length - 1;
+                if (!Array.isArray(rows[0]) && Array.isArray(rows[lastIndex]))
+                    rows = rows[lastIndex];
+
+                Object.defineProperty(rows, 'queryContext', {
+                    value: query.queryContext,
+                    enumerable: false
+                });
+                success(rows);
+            } else
+                failed(err);
+        }
+    }
 
 }
 
