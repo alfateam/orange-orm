d861e9e56951e479ba2dd828a63d844efbfebd8b
Lars-Erik Roald <lars.roald@gmail.com>
2017-02-19 22:32:49 +0100

    fixed integration tests with sqlite

--- tableSpec/integrationSpec/defineTable/includeLines/includePackages/newQuery.js
+++ tableSpec/integrationSpec/defineTable/includeLines/includePackages/newQuery.js
@@ -1,8 +1,7 @@
 function act(c) {
-
 	c.expected = "select order.oOrderId as sorder0,order.oCustomerId as sorder1 from order order where order.discriminatorColumn='foo' AND order.discriminatorColumn2='baz' order by order.oOrderId";
- 	c.expected2 = "select order_0.lId as sorder_00,order_0.lLineNo as sorder_01,order_0.lOrderId as sorder_02 from orderLine order_0 INNER JOIN order order ON (order_0.lOrderId=order.oOrderId AND order.discriminatorColumn='foo' AND order.discriminatorColumn2='baz') order by order.oOrderId";
-	c.expected3 = "select order_0_0.pId as sorder_0_00,order_0_0.pLineId as sorder_0_01,order_0_0.pArticleId as sorder_0_02 from package order_0_0 INNER JOIN orderLine order_0 ON (order_0_0.pLineId=order_0.lId) INNER JOIN order order ON (order_0.lOrderId=order.oOrderId AND order.discriminatorColumn='foo' AND order.discriminatorColumn2='baz') order by order_0.lId";
+ 	c.expected2 = "select order_0.lId as sorder_00,order_0.lLineNo as sorder_01,order_0.lOrderId as sorder_02 from orderLine order_0 INNER JOIN order order ON (order_0.lOrderId=order.oOrderId AND order.discriminatorColumn='foo' AND order.discriminatorColumn2='baz') order by order_0.lId";
+	c.expected3 = "select order_0_0.pId as sorder_0_00,order_0_0.pLineId as sorder_0_01,order_0_0.pArticleId as sorder_0_02 from package order_0_0 INNER JOIN orderLine order_0 ON (order_0_0.pLineId=order_0.lId) INNER JOIN order order ON (order_0.lOrderId=order.oOrderId AND order.discriminatorColumn='foo' AND order.discriminatorColumn2='baz') order by order_0_0.pId";
 	c.newQuery();	
 }
 
--- tableSpec/integrationSpec/defineTable/includeLines/newQueryWithFilterOrderByAndLimit.js
+++ tableSpec/integrationSpec/defineTable/includeLines/newQueryWithFilterOrderByAndLimit.js
@@ -3,7 +3,7 @@ function act(c) {
 				"where order.oOrderId=2 AND order.discriminatorColumn='foo' AND order.discriminatorColumn2='baz' "  + 
 				"order by order.oCustomerId desc limit 8";
 
-	c.expected2 = "select order_0.lId as sorder_00,order_0.lLineNo as sorder_01,order_0.lOrderId as sorder_02 from orderLine order_0 INNER JOIN (select * from order order where order.oOrderId=2 AND order.discriminatorColumn='foo' AND order.discriminatorColumn2='baz' order by order.oCustomerId desc limit 8) order ON (order_0.lOrderId=order.oOrderId AND order.discriminatorColumn='foo' AND order.discriminatorColumn2='baz') where order.oOrderId=2 order by order.oOrderId";
+	c.expected2 = "select order_0.lId as sorder_00,order_0.lLineNo as sorder_01,order_0.lOrderId as sorder_02 from orderLine order_0 INNER JOIN (select * from order order where order.oOrderId=2 AND order.discriminatorColumn='foo' AND order.discriminatorColumn2='baz' order by order.oCustomerId desc limit 8) order ON (order_0.lOrderId=order.oOrderId AND order.discriminatorColumn='foo' AND order.discriminatorColumn2='baz') where order.oOrderId=2 order by order_0.lId";
 
 	c.strategy.orderBy = 'customerId desc';
 	c.strategy.limit = 8;
