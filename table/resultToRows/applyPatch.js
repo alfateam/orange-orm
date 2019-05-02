61065b6d989aae68064952d0775cd4927934e167
Lars-Erik Roald <lars.roald@gmail.com>
2019-05-02 10:14:20 +0200

    applyPatch done add,remove and replace - wip:  hasMany

--- table/resultToRows/applyPatch.js
+++ table/resultToRows/applyPatch.js
@@ -3,26 +3,57 @@ async function applyPatch(row, table, changes, options) {
         var change = changes[index];
         if (change.op === 'replace') {
             var path = change.path.split('/');
-            await replace(row, table, path, change);           
+            await replace(row, table, path, change);
         }
         if (change.op === 'add') {
             var path = change.path.split('/');
             await add(row, table, path, change);
         }
+        if (change.op === 'remove') {
+            var path = change.path.split('/');
+            await remove(row, table, path, change);
+        }
+
     }
 
     async function replace(row, table, path, change) {
-        path.shift();        
+        path.shift();
         let prop = path[0];
-        if (path.length === 1) {     
+        if (path.length === 1) {
             let value = change.value;
             let childTable = table._relations[prop] && table._relations[prop].childTable;
             if (childTable) {
-                let primaryValues = childTable._primaryColumns.map(function(column) {
+                let primaryValues = childTable._primaryColumns.map(function (column) {
                     return value[column.alias];
                 });
                 let child = childTable.insert.apply(null, primaryValues);
-                for(var colName in value) {
+                for (var colName in value) {
+                    child[colName] = value[colName];
+                }
+            } else {
+                row[prop] = value;
+            }
+        } else {
+            let relation = table._relations[prop];
+            if (relation && relation.joinRelation) {
+                return replace(await row[prop], relation.childTable, path, change);
+            }
+        }
+    }
+
+    async function add(row, table, path, change) {
+        path.shift();
+        let prop = path[0];
+        console.log(prop)
+        if (path.length === 1) {
+            let value = change.value;
+            let childTable = table._relations[prop] && table._relations[prop].childTable;
+            if (childTable) {
+                let primaryValues = childTable._primaryColumns.map(function (column) {
+                    return value[column.alias];
+                });
+                let child = childTable.insert.apply(null, primaryValues);
+                for (var colName in value) {
                     child[colName] = value[colName];
                 }
             }
@@ -31,27 +62,32 @@ async function applyPatch(row, table, changes, options) {
             }
         }
         else {
-            let childTable = table._relations[prop] && table._relations[prop].childTable;
-            return replace(await row[prop], childTable, path, change);
-        }           
+            let relation = table._relations[prop];
+            if (relation && relation.joinRelation) {
+                return add(await row[prop], relation.childTable, path, change);
+            }
+        }
     }
 
-    async function add(row, table, path, {value}) {
-        path.shift();        
-        if (path.length === 1) {            
-            let prop = path[0];
+    async function remove(row, table, path, change) {
+        path.shift();
+        let prop = path[0];
+        if (path.length === 1) {
             let childTable = table._relations[prop] && table._relations[prop].childTable;
             if (childTable) {
-                let primaryValues = childTable._primaryColumns.map(function(column) {
-                    return value[column.alias];
-                });
-                let child = childTable.insert.apply(null, primaryValues);
-                for(var colName in value) {
-                    child[colName] = value[colName];
-                }
+                (await row[prop]).cascadeDelete();
+            } else {
+                row[prop] = undefined;
             }
-        }             
+        } else {
+            let relation = table._relations[prop];
+            if (relation && relation.joinRelation) {
+                return remove(await row[prop], relation.childTable, path, change);
+            }
+        }
     }
+
+
 }
 
 module.exports = applyPatch;
