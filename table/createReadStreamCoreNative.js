e86456dc7b1be445fdca8fb8a576f3a2ea8023d9
lroal <lars.roald@gmail.com>
2019-07-30 17:06:18 +0200

    native streaming working with async_hooks

--- table/createReadStreamCoreNative.js
+++ table/createReadStreamCoreNative.js
@@ -2,40 +2,36 @@ var newQuery = require('./readStream/newQuery');
 var strategyToSpan = require('./strategyToSpan');
 var negotiateRawSqlFilter = require('./column/negotiateRawSqlFilter');
 var newQueryStream = require('./readStream/newQueryStream');
-var deferred = require('deferred');
-var domain = require('domain');
 
 function createReadStreamCoreNative(table, db, filter, strategy, transformer, streamOptions) {
     var alias = table._dbName;
     filter = negotiateRawSqlFilter(filter);
     var span = strategyToSpan(table, strategy);
 
-    var originalDomain = process.domain || domain.create();
-    originalDomain.add(transformer);
+    if (process.domain)
+        process.domain.add(transformer);
 
-    var def = deferred();
-
-    db.transaction()
-        .then(start)
-        .then(db.commit)
-        .then(null, onError)
+    db.transaction(async () => {
+        await start();
+    }).then(null, onError);
 
     function start() {
-        var query = newQuery(db, table, filter, span, alias);
-        var queryStream = newQueryStream(query, streamOptions);
-        queryStream.on('end', def.resolve);
-        queryStream.on('error', onStreamError);
-        queryStream.pipe(transformer);
-        return def.promise;
-    }
+        return new Promise((resolve, reject) => {
+            var query = newQuery(db, table, filter, span, alias);
+            var queryStream = newQueryStream(query, streamOptions);
+            queryStream.on('end', resolve);
+            queryStream.on('error', onStreamError);
+            queryStream.pipe(transformer);
 
-    function onStreamError(e) {
-        def.reject(e);
+            function onStreamError(e) {
+                reject(e);
+            }
+        })
+
     }
 
     function onError(e) {
         transformer.emit('error', e);
-        db.rollback();
     }
 
     return transformer;
