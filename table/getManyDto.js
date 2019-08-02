590bf364344d82c3c16d46c2aa611f2d8f83e977
lroal <lars.roald@gmail.com>
2019-08-02 16:05:43 +0200

    GetManyDto for sqlite and mysql

--- table/getManyDto.js
+++ table/getManyDto.js
@@ -2,24 +2,46 @@ var newQuery = require('./getManyDto/newQuery');
 var strategyToSpan = require('./strategyToSpan');
 var negotiateRawSqlFilter = require('./column/negotiateRawSqlFilter');
 var executeQueries = require('./executeQueries');
+var getSessionContext = require('./getSessionContext');
 
 function getManyDto(table, filter, strategy) {
-    var alias = table._dbName;
-    filter = negotiateRawSqlFilter(filter);
-    var span = strategyToSpan(table, strategy);
-    var query = newQuery(table, filter, span, alias);
-    return executeQueries([query]).then(onResults).then(onSingleResult);
+	let isPg;
+	let c = {};
+	c.visitPg = function() {
+		isPg = true;
+	};
+	c.visitMySql = function() {};
+	c.visitSqlite = function() {};
 
-    function onResults(rowsPromises) {
-        return rowsPromises[0];
-    }
+	getSessionContext().accept(c);
 
-    function onSingleResult(result) {
-        for(var p in result[0]) {
-            let res =  result[0][p];
-            return res;
-        }
-    }
+	if (!isPg) {
+		let args = [];
+		for (var i = 1; i < arguments.length; i++) {
+			args.push(arguments[i]);
+		}
+		return table.getMany.apply(null, args)
+			.then((rows) => {
+				args.shift();
+				return rows.toDto.apply(null, args);
+			});
+	}
+	var alias = table._dbName;
+	filter = negotiateRawSqlFilter(filter);
+	var span = strategyToSpan(table, strategy);
+	var query = newQuery(table, filter, span, alias);
+	return executeQueries([query]).then(onResults).then(onSingleResult);
+
+	function onResults(rowsPromises) {
+		return rowsPromises[0];
+	}
+
+	function onSingleResult(result) {
+		for (var p in result[0]) {
+			let res = result[0][p];
+			return res;
+		}
+	}
 
 }
 
