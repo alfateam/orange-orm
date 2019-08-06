e99aa9173a84b90ecde88d94aea8d0fe1a393285
lroal <lars.roald@gmail.com>
2019-08-06 10:39:50 +0200

    Using generic-pool v2 instead of v3 which has bugs and keeps node program hanging.

--- sqlite/pool/newGenericPool.js
+++ sqlite/pool/newGenericPool.js
@@ -1,71 +1,56 @@
-var promise = require('../../table/promise');
 var EventEmitter = require('events').EventEmitter;
 
 var defaults = require('./defaults');
-var genericPool = require('generic-pool');
+var genericPool = require('../../generic-pool');
 var sqlite = require('sqlite3');
 
 function newGenericPool(connectionString, poolOptions) {
 	poolOptions = poolOptions || {};
-	var factory = {
-		create: function() {
-			return promise(function(resolve, reject) {
-				var client = new sqlite.Database(connectionString, onConnected);
-
-				function onConnected(err) {
-					if (err)
-						return reject(err);
-					client.poolCount = 0;
-					resolve(client);
-				}
-			});
-		},
-		destroy: function(client) {
-			return promise(function(resolve, reject) {
-				try {
-					client.poolCount = undefined;
-					client.close();
-					resolve();
-				} catch (e) {
-					reject(e);
-				}
-			});
-
-		}
-	};
-	var pool = genericPool.createPool(factory, {
+	var pool = genericPool.Pool({
 		max: poolOptions.size || poolOptions.poolSize || defaults.poolSize,
 		idleTimeoutMillis: poolOptions.idleTimeout || defaults.poolIdleTimeout,
 		reapIntervalMillis: poolOptions.reapIntervalMillis || defaults.reapIntervalMillis,
 		log: poolOptions.log || defaults.poolLog,
+		create: function(cb) {
+			var client = new sqlite.Database(connectionString, onConnected);
+
+			function onConnected(err) {
+				if(err)
+					return cb(err, null);
+				client.poolCount = 0;
+				return cb(null, client);
+			}
+		},
+
+		destroy: function(client) {
+			client.poolCount = undefined;
+			client.close();
+		}
 	});
 	//mixin EventEmitter to pool
 	EventEmitter.call(pool);
-	for (var key in EventEmitter.prototype) {
-		if (EventEmitter.prototype.hasOwnProperty.call(EventEmitter, key)) {
+	for(var key in EventEmitter.prototype) {
+		if(EventEmitter.prototype.hasOwnProperty(key)) {
 			pool[key] = EventEmitter.prototype[key];
 		}
 	}
 	//monkey-patch with connect method
 	pool.connect = function(cb) {
 		var domain = process.domain;
-		if (domain) {
-			cb = domain.bind(cb);
-		}
-		pool.acquire()
-			.then(function(client) {
-				client.poolCount++;
-				cb(null, client, function(err) {
-					if (err) {
-						pool.destroy(client);
-					} else {
-						pool.release(client);
-					}
-				});
-			})
-			.then(null, function(err) {
-				cb(err, null, function() {});
+		pool.acquire(function(err, client) {
+			if(domain) {
+				cb = domain.bind(cb);
+			}
+			if(err)  return cb(err, null, function() {/*NOOP*/});
+			client.poolCount++;
+			cb(null, client, function(err) {
+				if(err) {
+					pool.destroy(client);
+				} else {
+					pool.release(client);
+				}
 			});
+		});
 	};
 	return pool;
 }
