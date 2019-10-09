9375c37a1b4c89fff24941ae5cef70ccd8fad5d5
Lars-Erik Roald <lars.roald@gmail.com>
2019-10-09 15:40:38 +0200

    bugfix insert and hasOne Relation

--- mySql/newTransactionSpec/new/runDomain/resolveError.js
+++
@@ -1,8 +0,0 @@
-function act(c){
-	c.err = {};	
-	c.onError.expect(c.err);
-
-	c.raiseConnected(c.err);
-}
-
-module.exports = act;
--- pg/newTransactionSpec/new/runDomain/whenResolveError.js
+++
@@ -1,6 +0,0 @@
-var when = require('a').when;
-var c = {};
-
-when(c)
-	.it('should not set rdb').assertEqual(false, 'rdb' in c.domain)
-    .it('should invoke onError callback').assertDoesNotThrow(c.onError.verify)
