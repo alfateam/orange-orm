43dac847e3d2e689e191dc3e006a27eba234f0df
Lars-Erik Roald <lars.roald@gmail.com>
2020-03-15 21:34:21 +0100

    wip patch many

--- applyPatch.js
+++ applyPatch.js
@@ -12,32 +12,40 @@ async function applyPatch(table, patches) {
 			await remove(patch, table);
 	}
 
-
-	async function add({path, value, op}, table, row, parentRow) {
+	async function add({path, value, op}, table, row, parentRow, relation) {
 		let property = path[0];
 		path = path.slice(1);
-		row = row || await table.getById(property);
-		if (path.length === 0)
-			return;//insert
-			// return row.cascadeDelete();
+		if (!relation)
+			row = row || await table.tryGetById(property);
+		// if ((path.length === 0) && !relation) {
+
+		// }
+		if (path.length === 0) {
+			let pkName = table._primaryColumns[0].alias;
+			row = table.insert(property[pkName]);
+			for(let name in value) {
+				row[name] = value[name];
+			}
+			return;
+		}
 		property = path[0];
-		console.log(property);
 		if (isColumn(property, table)) {
+			console.log('isColumn');
 			dto = {};
 			dto[property] = row[property];
 			row[property] = applyPatchToColumn({}, dto, [{path: '/' + path.join('/'), op, value}]);
 		}
 		else if (isOneRelation(property, table)) {
-			let child = await row[property];
-			if (!child)
-				throw new Error(property + ' does not exist');
-			await add({path, value, op}, table[property], child);
+			console.log('isOne');
+			let relation = table[property]._relation;
+			await add({path, value, op}, relation.childTable, await row[property], row, relation);
 		}
 		else if (isManyRelation(property, table)) {
+			console.log('isMany');
 			let children = (await row[property]).slice(0);
 			for (let i = 0; i < children.length; i++) {
-				let child = children[i];
-				await add({path, value, op}, table[property], child);
+				let relation = table[property]._relation;
+				await add({path, value, op}, relation.childTable, await row[property], row, relation);
 			}
 		}
 	}
