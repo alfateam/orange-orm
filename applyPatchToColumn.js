f54f2707b552f460a50a736980ef3dabc6f85129
Lars-Erik Roald <lars.roald@gmail.com>
2020-08-13 15:28:33 +0200

    removed unused

--- applyPatchToColumn.js
+++ applyPatchToColumn.js
@@ -21,7 +21,6 @@ function applyPatch(options = {}, dto, changes) {
 	return dto;
 
 	function validateConflict(object, changes) {
-		const set = [];
 		for (let i = 0; i < changes.length; i++) {
 			let change = changes[i];
 			let expectedOldValue = change.oldValue;
