e47885f02fdf4a2d9ccd592465e2225f16416ef1
lroal <lars.roald@gmail.com>
2020-09-21 11:06:51 +0200

    log delete

--- table/resultToRows/delete.js
+++ table/resultToRows/delete.js
@@ -2,6 +2,8 @@ var removeFromCache = require('./delete/removeFromCache');
 var pushCommand = require('../commands/pushCommand');
 var newDeleteCommand = require('../commands/newDeleteCommand');
 var newPrimaryKeyFilter = require('../newPrimaryKeyFilter');
+var createPatch = require('rdb-client').createPatch;
+var createDto = require('./toDto/createDto');
 
 function _delete(row, strategy, table) {
 	var relations = [];
@@ -16,6 +18,14 @@ function _delete(row, strategy, table) {
 	cmds.forEach(function(cmd) {
 		pushCommand(cmd);
 	});
+	var cmd = cmds[0];
+	if (table._emitChanged.callbacks.length > 0) {
+		cmd.disallowCompress = true;
+		var dto = createDto(table, row);
+		let patch =  createPatch([dto],[]);
+		cmd.emitChanged = table._emitChanged.bind(null, {row: row, patch: patch});
+	}
+
 }
 
 module.exports = _delete;
