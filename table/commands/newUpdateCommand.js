b8cdb6487e9262269e59295d82911e075abf1475
lroal <lars.roald@gmail.com>
2020-10-21 08:49:57 +0200

    bugfix missing onUpdate data

--- table/commands/newUpdateCommand.js
+++ table/commands/newUpdateCommand.js
@@ -1,19 +1,21 @@
-var newUpdateCommandCore = require('./newUpdateCommandCore');
-var newImmutable = require('../../newImmutable');
-var newColumnList = require('../../newObject');
+let newUpdateCommandCore = require('./newUpdateCommandCore');
+let newImmutable = require('../../newImmutable');
+let newColumnList = require('../../newObject');
+let createPatch = require('rdb-client').createPatch;
+let createDto = require('../resultToRows/toDto/createDto');
 
 function newUpdateCommand(table, column, row) {
-    return new UpdateCommand(table, column, row);
+	return new UpdateCommand(table, column, row);
 }
 
 function UpdateCommand(table, column, row) {
-    this._table = table;
-    this._row = row;
-    this.__getCoreCommand = newImmutable(newUpdateCommandCore);
-    this._columnList = newColumnList();
-    this._columnList[column.alias] = column;
-    this.onFieldChanged = this.onFieldChanged.bind(this)
-    row.subscribeChanged(this.onFieldChanged);
+	this._table = table;
+	this._row = row;
+	this.__getCoreCommand = newImmutable(newUpdateCommandCore);
+	this._columnList = newColumnList();
+	this._columnList[column.alias] = column;
+	this.onFieldChanged = this.onFieldChanged.bind(this);
+	row.subscribeChanged(this.onFieldChanged);
 }
 
 UpdateCommand.prototype.onFieldChanged = function(row, column) {
@@ -35,8 +37,15 @@ UpdateCommand.prototype._getCoreCommand = function() {
 };
 
 UpdateCommand.prototype.endEdit = function() {
-    this._getCoreCommand();
-    this._row.unsubscribeChanged(this.onFieldChanged);
+	this._getCoreCommand();
+	this._row.unsubscribeChanged(this.onFieldChanged);
+	let dto = JSON.parse(JSON.stringify(createDto(this._table, this._row)));
+	this._patch = createPatch([JSON.parse(this._row._oldValues)],[dto]);
+	this._row._oldValues = JSON.stringify(dto);
+};
+
+UpdateCommand.prototype.emitChanged = function() {
+	return this._table._emitChanged({row: this._row, patch: this._patch});
 };
 
 UpdateCommand.prototype.matches = function(otherRow) {
--- table/resultToRows/newDecodeDbRow.js
+++ table/resultToRows/newDecodeDbRow.js
@@ -1,155 +1,166 @@
-var updateField = require('../updateField');
-var newEmitEvent = require('../../emitEvent');
-var extractStrategy = require('./toDto/extractStrategy');
-var extractDeleteStrategy = require('../extractDeleteStrategy');
-var newCascadeDeleteStrategy = require('../newCascadeDeleteStrategy');
-var _delete = require('./delete');
-var newObject = require('../../newObject');
-var toDto = require('./toDto');
-var patchRow = require('../../patchRow');
+let updateField = require('../updateField');
+let newEmitEvent = require('../../emitEvent');
+let extractStrategy = require('./toDto/extractStrategy');
+let extractDeleteStrategy = require('../extractDeleteStrategy');
+let newCascadeDeleteStrategy = require('../newCascadeDeleteStrategy');
+let _delete = require('./delete');
+let newObject = require('../../newObject');
+let toDto = require('./toDto');
+let createDto = require('./toDto/createDto');
+let patchRow = require('../../patchRow');
 
 function newDecodeDbRow(table, dbRow) {
-    var columns = table._columns;
-    var numberOfColumns = columns.length;
-    if (dbRow.offset === undefined) {
-        dbRow.offset = 0;
-    }
+	let columns = table._columns;
+	let numberOfColumns = columns.length;
+	if (dbRow.offset === undefined) {
+		dbRow.offset = 0;
+	}
 
-    var offset = dbRow.offset;
+	let offset = dbRow.offset;
 
-    var keys = Object.keys(dbRow);
+	let keys = Object.keys(dbRow);
 
-    for (var i = 0; i < numberOfColumns; i++) {
-        defineColumnProperty(i);
-    }
+	for (let i = 0; i < numberOfColumns; i++) {
+		defineColumnProperty(i);
+	}
 
-    dbRow.offset += numberOfColumns;
+	dbRow.offset += numberOfColumns;
 
-    function defineColumnProperty(i) {
-        var column = columns[i];
-        var purify = column.purify;
-        var name = column.alias;
-        i = offset + i;
-        var key = keys[i];
+	function defineColumnProperty(i) {
+		let column = columns[i];
+		let purify = column.purify;
+		let name = column.alias;
+		i = offset + i;
+		let key = keys[i];
 
-        Object.defineProperty(Row.prototype, name, {
+		Object.defineProperty(Row.prototype, name, {
 
-            get: function() {
-                return this._dbRow[key];
-            },
-            set: function(value) {
-                var oldValue = this[name];
-                value = purify(value);
-                this._dbRow[key] = value;
-                updateField(table, column, this);
-                var emit = this._emitColumnChanged[name];
-                if (emit)
-                    emit(this, column, value, oldValue);
-                this._emitChanged(this, column, value, oldValue);
-            }
-        });
-    }
+			get: function() {
+				return this._dbRow[key];
+			},
+			set: function(value) {
+				let oldValue = this[name];
+				value = purify(value);
+				this._dbRow[key] = value;
+				if (column.validate)
+					column.validate(value, this._dbRow);
+				updateField(table, column, this);
+				let emit = this._emitColumnChanged[name];
+				if (emit)
+					emit(this, column, value, oldValue);
+				this._emitChanged(this, column, value, oldValue);
+			}
+		});
+	}
 
-    setRelated();
+	setRelated();
 
-    function setRelated() {
-        var relations = table._relations;
-        for (var relationName in relations) {
-            var relation = relations[relationName];
-            setSingleRelated(relationName, relation);
-        }
-    }
+	function setRelated() {
+		let relations = table._relations;
+		for (let relationName in relations) {
+			let relation = relations[relationName];
+			setSingleRelated(relationName, relation);
+		}
+	}
 
-    function setSingleRelated(name, relation) {
-        Object.defineProperty(Row.prototype, name, {
-            get: function() {
-                return createGetRelated(this, name)();
-            }
-        });
-    }
+	function setSingleRelated(name) {
+		Object.defineProperty(Row.prototype, name, {
+			get: function() {
+				return createGetRelated(this, name)();
+			}
+		});
+	}
 
-    function createGetRelated(row, alias) {
-        var get = row._related[alias];
-        if (!get) {
-            var relation = table._relations[alias];
-            get = relation.toGetRelated(row);
-            row._related[alias] = get;
-        }
-        return get;
-    }
+	function createGetRelated(row, alias) {
+		let get = row._related[alias];
+		if (!get) {
+			let relation = table._relations[alias];
+			get = relation.toGetRelated(row);
+			row._related[alias] = get;
+		}
+		return get;
+	}
 
+	Row.prototype.subscribeChanged = function(onChanged, name) {
+		let emit;
+		if (name) {
+			emit = this._emitColumnChanged[name] || (this._emitColumnChanged[name] = newEmitEvent());
+			emit.add(onChanged);
+			return;
+		}
+		this._emitChanged.add(onChanged);
+	};
 
-    Row.prototype.subscribeChanged = function(onChanged, name) {
-        var emit;
-        if (name) {
-            emit = this._emitColumnChanged[name] || (this._emitColumnChanged[name] = newEmitEvent());
-            emit.add(onChanged);
-            return
-        }
-        this._emitChanged.add(onChanged);
-    };
+	Row.prototype.unsubscribeChanged = function(onChanged, name) {
+		if (name) {
+			this._emitColumnChanged[name].tryRemove(onChanged);
+			return;
+		}
+		this._emitChanged.tryRemove(onChanged);
+	};
 
-    Row.prototype.unsubscribeChanged = function(onChanged, name) {
-        if (name) {
-            this._emitColumnChanged[name].tryRemove(onChanged);
-            return;
-        }
-        this._emitChanged.tryRemove(onChanged);
-    };
+	Row.prototype.toJSON = function() {
+		return this.toDto.apply(this, arguments).then(JSON.stringify);
+	};
 
-    Row.prototype.toJSON = function(strategy) {
-        return this.toDto.apply(this, arguments).then(JSON.stringify);
-    };
+	Row.prototype.toDto = function(strategy) {
+		let args = Array.prototype.slice.call(arguments, 0);
+		args.push(table);
+		strategy = extractStrategy.apply(null, args);
+		let p =  toDto(strategy, table, this);
+		return Promise.resolve().then(() => p);
+	};
 
-    Row.prototype.toDto = function(strategy) {
-        var args = Array.prototype.slice.call(arguments, 0);
-        args.push(table);
-        strategy = extractStrategy.apply(null, args);
-        return toDto(strategy, table, this);
-    };
+	Row.prototype.__toDto = function(strategy) {
+		let args = Array.prototype.slice.call(arguments, 0);
+		args.push(table);
+		strategy = extractStrategy.apply(null, args);
+		return toDto(strategy, table, this);
+	};
 
-    Row.prototype.expand = function(alias) {
-        var get = createGetRelated(this, alias);
-        get.expanded = true;
-    };
+	Row.prototype.expand = function(alias) {
+		let get = createGetRelated(this, alias);
+		get.expanded = true;
+	};
 
-    Row.prototype.isExpanded = function(alias) {
-        var get = createGetRelated(this, alias);
-        return get.expanded;
-    };
+	Row.prototype.isExpanded = function(alias) {
+		let get = createGetRelated(this, alias);
+		return get.expanded;
+	};
 
-    Row.prototype.delete = function(strategy) {
-        strategy = extractDeleteStrategy(strategy, table);
-        _delete(this, strategy, table);
-    };
+	Row.prototype.delete = function(strategy) {
+		strategy = extractDeleteStrategy(strategy, table);
+		_delete(this, strategy, table);
+	};
 
-    Row.prototype.cascadeDelete = function() {
-        var strategy = newCascadeDeleteStrategy(newObject(), table);
-        _delete(this, strategy, table);
-    };
+	Row.prototype.cascadeDelete = function() {
+		let strategy = newCascadeDeleteStrategy(newObject(), table);
+		_delete(this, strategy, table);
+	};
 
-    Row.prototype.patch = async function(patches, options) {
-        await patchRow(table, this, patches, options);
-        return this;
-    };
+	Row.prototype.patch = async function(patches, options) {
+		await patchRow(table, this, patches, options);
+		return this;
+	};
 
-    function decodeDbRow(row) {
-        for (var i = 0; i < numberOfColumns; i++) {
-            var index = offset + i;
-            var key = keys[index];
-            row[key] = columns[i].decode(row[key]);
-        }
-        return new Row(row);
-    }
+	function decodeDbRow(row) {
+		for (let i = 0; i < numberOfColumns; i++) {
+			let index = offset + i;
+			let key = keys[index];
+			row[key] = columns[i].decode(row[key]);
+		}
+		return new Row(row);
+	}
 
-    function Row(dbRow) {
-        this._dbRow = dbRow;
-        this._related = {};
-        this._emitColumnChanged = {};
-        this._emitChanged = newEmitEvent();
-    }
+	function Row(dbRow) {
+		this._dbRow = dbRow;
+		this._oldValues =  JSON.stringify(createDto(table, this));
+		this._related = {};
+		this._emitColumnChanged = {};
+		this._emitChanged = newEmitEvent();
+	}
 
-    return decodeDbRow;
+	return decodeDbRow;
 }
 
 module.exports = newDecodeDbRow;
