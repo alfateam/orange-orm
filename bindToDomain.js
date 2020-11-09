f382aa5309ee3995822313cd858d49644a748a52
Lars-Erik Roald <lars.roald@gmail.com>
2020-11-09 15:01:59 +0100

    bind to AsyncResource workaround

--- bindToDomain.js
+++ bindToDomain.js
@@ -1,12 +1,28 @@
 let useHook = require('./useHook');
 let AsyncResource = useHook && require('async_hooks').AsyncResource;
+
 function bindToDomain(callback) {
+	if (AsyncResource.bind && AsyncResource.bind !== Function.prototype.bind)
+		callback = bindWorkAround(callback);
 	if (process.domain)
 		callback = process.domain.bind(callback);
-	if (AsyncResource.bind) {
-		callback = AsyncResource.bind(callback);
-	}
 	return callback;
+
+}
+
+function bindWorkAround(cb) {
+	AsyncResource.bind(invokeOriginal);
+	let _arguments;
+	return onData;
+
+	function onData() {
+		_arguments = arguments;
+		invokeOriginal();
+	}
+
+	function invokeOriginal() {
+		cb.apply(null, _arguments);
+	}
 
 }
 
