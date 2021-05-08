b12199b0266da59e8b48a52d3804f000870eb071
Lars-Erik Roald <lars.roald@gmail.com>
2021-05-08 17:02:43 +0200

    working version with getManyDto from browser

--- hostExpress.js
+++ hostExpress.js
@@ -1,22 +1,23 @@
 let express;
 let query = require('./query');
 
-function hostExpress({ db, table, defaultConcurrency, concurrency }) {
+function hostExpress(options) {
+	let { db, table, tables = [], allowSql } = options;
 	let router = express.Router();
-	if (table)
+	if (table) {
 		router.patch('/', async function(req, res) {
 			try {
 				if (typeof db === 'function') {
 					let dbPromise = db();
 					if (dbPromise.then)
-						// eslint-disable-next-line require-atomic-updates
+					// eslint-disable-next-line require-atomic-updates
 						db = await dbPromise;
 					else
 						db = dbPromise;
 				}
 				await db.transaction(async() => {
 					let patch = req.body;
-					await table.patch(patch, { defaultConcurrency, concurrency });
+					await table.patch(patch, options);
 				});
 				res.status(204).send();
 			}
@@ -24,7 +25,13 @@ function hostExpress({ db, table, defaultConcurrency, concurrency }) {
 				res.status(500).send(e && e.message);
 			}
 		});
+	}
 	else {
+		let tablesDictionary = {};
+		for (let i = 0; i < tables.length; i++) {
+			let table = tables[i];
+			tablesDictionary[table._dbName] = table;
+		}
 		router.post('/', async function(req, res) {
 			try {
 				if (typeof db === 'function') {
@@ -36,11 +43,21 @@ function hostExpress({ db, table, defaultConcurrency, concurrency }) {
 						db = dbPromise;
 				}
 				let result;
+				let body = req.body;
+				let table = tablesDictionary[body.table];
+				if (!table)
+					throw new Error(`Table ${body.table} is not exposed`);
+				//todo
+				// let columnDiscriminators = body.columnDiscriminators;
+				// let formulaDiscriminators = body.formulaDiscriminators;
+				let filter = body.sqlWhere && {
+					sql: body.sqlWhere,
+					parameters: body.parameters
+				};
+				let strategy = body.strategy;
+
 				await db.transaction(async() => {
-					result = await query({
-						query: req.body.query,
-						parameters: req.body.parameters
-					});
+					result = await table.getManyDto(filter, strategy);
 				});
 				res.json(result);
 			}
@@ -48,6 +65,32 @@ function hostExpress({ db, table, defaultConcurrency, concurrency }) {
 				res.status(500).send(e && e.message);
 			}
 		});
+
+
+		if (allowSql)
+			router.put('/', async function(req, res) {
+				try {
+					if (typeof db === 'function') {
+						let dbPromise = db();
+						if (dbPromise.then)
+							// eslint-disable-next-line require-atomic-updates
+							db = await dbPromise;
+						else
+							db = dbPromise;
+					}
+					let result;
+					await db.transaction(async() => {
+						result = await query({
+							sql: req.body.sql,
+							parameters: req.body.parameters
+						});
+					});
+					res.json(result);
+				}
+				catch (e) {
+					res.status(500).send(e && e.message);
+				}
+			});
 
 	}
 	return router;
