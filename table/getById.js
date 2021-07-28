336bf2e5832686afa14c0987d584ce7cee713d48
Lars-Erik Roald <lars.roald@gmail.com>
2021-07-28 12:28:34 +0200

    getById should go to db unless whole strategy is cached

--- table/getById.js
+++ table/getById.js
@@ -1,14 +1,33 @@
-var tryGetFromCacheById = require('./tryGetFromCacheById');
-var getFromDbById = require('./getFromDbById');
-var resultToPromise = require('./resultToPromise');
+let tryGetFromCacheById = require('./tryGetFromCacheById');
+let getFromDbById = require('./getFromDbById');
+let resultToPromise = require('./resultToPromise');
+let extractStrategy = require('./tryGetFromDbById/extractStrategy');
 
-function getById() {
-	var cached =  tryGetFromCacheById.apply(null,arguments);
-	if (cached)
+async function getById() {
+	let strategy = extractStrategy.apply(null, arguments);
+	let cached =  tryGetFromCacheById.apply(null,arguments);
+	if (cached) {
+		await expand(cached, strategy);
 		return resultToPromise(cached);
+
+	}
 	return getFromDbById.apply(null,arguments);
 }
 
 getById.exclusive = getFromDbById.exclusive;
+
+async function expand(rows, strategy) {
+	if (!rows)
+		return;
+	if (!Array.isArray(rows))
+		rows = [rows];
+	for(let p in strategy) {
+		if(!(strategy[p] === null || strategy[p]))
+			continue;
+		for (let i = 0; i < rows.length; i++) {
+			await expand(await rows[i][p], strategy[p]);			
+		}
+	}
+}
 
 module.exports = getById;
