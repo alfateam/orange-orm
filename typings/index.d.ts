d14778fd30681a2870e7fea892fddded48f11960
Lars-Erik Roald <lars.roald@systor.st>
2021-10-05 16:02:36 +0200

    typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,10 +1,50 @@
+
 export interface Rdb {
     table(name: string): Table;
-}      
+    end(): Promise<void>;
+    pg(connectionString : string, options?: PoolOptions) : Pool;
+    sqlite(connectionString : string, options?: PoolOptions) : Pool;
+    sap(connectionString : string, options?: PoolOptions) : Pool;
+    mssql(connectionString : string, options?: PoolOptions) : Pool;
+    mySql(connectionString : string, options?: PoolOptions) : Pool;
+}
+
+export interface Pool {
+    end(): Promise<void>;
+}
+
+export interface PoolOptions {
+    size?: number;
+}
+
+export interface Join {
+    by(...columns: string[]) : JoinBy;
+}
+
+export interface JoinBy {
+    as(propertyName: string): JoinRelation;
+}
+
+export interface JoinRelation{
+
+}
+
 
 export interface Table {
     primaryColumn(column: string) : Column;
     column(column: string) : Column;
+    join(table: Table) : Join;
+    hasMany(join: JoinRelation) : HasMany;
+    hasOne(join: JoinRelation) : HasOne;
+}
+
+interface HasMany {
+    as(propertyName: string): void;
+}
+
+
+interface HasOne {
+    as(propertyName: string): void;
 }
 
 export interface Column {
@@ -14,7 +54,7 @@ export interface Column {
     guid(): ColumnOf<string>;
     binary(): ColumnOf<Buffer>;
     boolean(): ColumnOf<boolean>;
-    json(): ColumnOf<object>;
+    json(): ColumnOf<Record<string,unknown>>;
     date(): DateColumn;
 }
 export interface DateColumn {
@@ -26,8 +66,8 @@ export interface DateColumn {
 
 export interface ColumnOf<T> {
     serializable(value: boolean): ColumnOf<T>;
-    default(value: T | (() => T)): ColumnOf<T>
-    dbNull(value: T | null) : ColumnOf<T>
+    default(value: T | (() => T)): ColumnOf<T>;
+    dbNull(value: T | null) : ColumnOf<T>;
 
 }
 
