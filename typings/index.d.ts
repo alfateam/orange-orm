88a6a3173df55f636d905be5d76e3562a52e3ad5
Lars-Erik Roald <lars.roald@systor.st>
2021-10-05 08:42:11 +0200

    ts types: columns

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,9 +1,34 @@
 export interface Rdb {
-    table(name: string): table;
+    table(name: string): Table;
 }      
 
-export interface table {
-    
+export interface Table {
+    primaryColumn(column: string) : Column;
+    column(column: string) : Column;
+}
+
+export interface Column {
+    serializable(value: boolean): Column;
+    string(): ColumnOf<string>;
+    numeric(): ColumnOf<number>;
+    guid(): ColumnOf<string>;
+    binary(): ColumnOf<Buffer>;
+    boolean(): ColumnOf<boolean>;
+    json(): ColumnOf<object>;
+    date(): DateColumn;
+}
+export interface DateColumn {
+    serializable(value: boolean): DateColumn;
+    as(dbName: string) : DateColumn;
+    default(value: Date | string |  (() => Date | string)): DateColumn
+    dbNull(value: Date | string | null) : DateColumn;
+}
+
+export interface ColumnOf<T> {
+    serializable(value: boolean): ColumnOf<T>;
+    default(value: T | (() => T)): ColumnOf<T>
+    dbNull(value: T | null) : ColumnOf<T>
+
 }
 
 declare const rdb: Rdb;
