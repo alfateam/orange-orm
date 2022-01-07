ad8b18297ca85c27bb7975626f4d17fcfac536a3
Lars-Erik Roald <lars.roald@systor.st>
2022-01-07 13:33:53 +0100

    Upgraded eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-	"env": {
-		"node": true,
-		"es6": true
+	'env': {
+		'node': true,
+		'es6': true
 	},
-	"extends": [
-		"eslint:recommended"
+	'extends': [
+		'eslint:recommended'
 	],
-	"parserOptions": {
-		"sourceType": "module",
-		"ecmaVersion": 2018
+	'parserOptions': {
+		'sourceType': 'module',
+		'ecmaVersion': 2018
 	},
-	"rules": {
-		"indent": ["error", "tab"],
-		"linebreak-style": ["error", "unix"],
-		"quotes": ["error", "single"],
-		"semi": ["error", "always"],
-		"space-before-function-paren": ["error", "never"],
-		"no-console": "off",
-		"no-debugger": "off",
-		"no-trailing-spaces": "error",
-		"no-underscore-dangle": 0,
-		"no-unused-vars": [2, { "args": "after-used", "ignoreRestSiblings": true,"argsIgnorePattern": "^_"}]
+	'rules': {
+		'indent': ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		'quotes': ['error', 'single'],
+		'semi': ['error', 'always'],
+		'space-before-function-paren': ['error', 'never'],
+		'no-console': 'off',
+		'no-debugger': 'off',
+		'no-trailing-spaces': 'error',
+		'no-underscore-dangle': 0,
+		'no-unused-vars': [2, { 'args': 'after-used', 'ignoreRestSiblings': true,'argsIgnorePattern': '^_'}]
 	},
-}
+};
