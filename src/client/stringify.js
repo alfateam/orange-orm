b65be241bd03add2218593cb1f46d21a975adeb6
Lars-Erik Roald <lars.roald@systor.st>
2022-11-17 12:20:06 +0100

    buffer replacer

--- src/client/stringify.js
+++ src/client/stringify.js
@@ -1,18 +1,18 @@
-let isStringifying = false;
 let dateToIsoString = require('../dateToIsoString');
 
-let dateToJSON = Date.prototype.toJSON;
-Date.prototype.toJSON = function() {
-	if (isStringifying)
-		return dateToIsoString(this);
-	return dateToJSON.apply(this);
-};
 
 function stringify(value) {
-	isStringifying = true;
-	let result = JSON.stringify(value);
-	isStringifying = false;
-	return result;
+	return JSON.stringify(value, replacer);
+}
+
+function replacer(value) {
+	if (Buffer.isBuffer(value))
+		return value.toString('base64');
+	// @ts-ignore
+	else if (value instanceof Date  && !isNaN(value))
+		return dateToIsoString(value);
+	else
+		return value;
 }
 
 module.exports = stringify;
