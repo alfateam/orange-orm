258edffc531c4bd4f6e6142f7058a528d1491d7b
Lars-Erik Roald <lars.roald@systor.st>
2023-03-02 12:33:19 +0000

    cleanup

--- tests/exp.ts
+++ tests/exp.ts
@@ -1,43 +1,16 @@
 import { Filter } from "../typings"
 
-
-
-type TableMap = Record<string, NColumnMap<ColumnTypes>>
-
-type ReferenceMap<TRelated> = {
-	// exists(): Filter
-}
-
-
-
-// type Mapped<T> = {
-
-// }
-type Mapper = {
-	column(name: string): ColumnMap
-}
 interface Rdb2 {
-	table<T>(fn: (mapper: Mapper) => TableMapOf<T>): Table<T, TableMapOf<T>>
-	table2<T>(dbName: string): TableBuilder<T>;
+	table<T>(dbName: string): TableBuilder<T>;
 }
 
-
-// type Table<T, TTableMap extends TableMapOf<T>> = {
-// & {
-// 	getAll() : Promise<T[]>;
-// }
 
 type TableBuilder<T> = {
 	map<TMap extends TableMapOf<T>>(map: TMap): Table<T, TMap>
 }
 
 type TableMapOf<T> = {
-	// [key in keyof T]-?: T[key] extends string ? StringColumnMap : (T[key] extends infer U extends Table<U, infer UMap>?  Table<U, UMap> : never)
 	[key in keyof T]-?: T[key] extends string ? StringColumnMap : any;
-	//  [key in keyof T]-?: StringColumnMap | NumberColumnMap | DateColumnMap | UuidColumnMap | BinaryColumnMap | BooleanColumnMap | any 
-	//  (T[key]  extends Table<infer RTable, infer TTableMap> & TableMapOf<infer RTable> ?  Table<infer RTable, infer TTableMap> & TableMapOf<infer RTable> : never)
-	//  (T[key]  extends TableMapOf<infer Related> ? TableMapOf<infer Related>: never)
-	//  T[key] extends infer U extends TableMapOf<U> ? ReferenceMap<U> : never
 };
 
 
@@ -62,33 +35,23 @@ type ChildTable = {
 //@ts-ignore
 const bar: Rdb2 = {};
 
-// type MappedUser = {
-// 	id: mapper.column('id').string(),		
-// 	email: mapper.column('email').string(),
-// }
-const customer = bar.table2<Customer>('customer').map({
-	// customerId: {columnType: 'string'},		
+const line = bar.table<Line>('line').map({
+	lineId: {columnType: 'string'}	
+});
+const customer = bar.table<Customer>('customer').map({
 	customerId: { columnType: 'string' },
 	name: { columnType: 'string' },
+	line: line
 });
 
 
-
-const user = bar.table2<User>('user').map({
+const user = bar.table<User>('user').map({
 	id: { columnType: 'string' },
 	email: { columnType: 'string' },
 	customer: customer
 });
-user.getAll
 
-
-
-// type ChildTable<TTableMap> = {
-// 	// [key in keyof TTableMap]-?: TTableMap[key] extends StringColumnMap ? StringColumn: (TTableMap[key] extends infer U extends TableMapOf<infer Child> ? ChildTable<U> : never) ;
-// }
-// // & {
-// // 	exists() : Filter;
-// // }
+// user.customer.
 
 type StringColumn = {
 	equals(string: string): Filter;
@@ -104,9 +67,6 @@ interface ColumnMap {
 	boolean(): BooleanColumnMap,
 }
 
-// interface ReferenceMap<TRow> extends NColumnMap<'string'> {
-
-// }
 
 interface StringColumnMap extends NColumnMap<'string'> {
 
@@ -161,15 +121,6 @@ type NColumnMap<TColumnType extends ColumnTypes> = {
 	columnType: TColumnType
 }
 
-
-
-
-
-
-
-
-
-
 class User {
 	id: string;
 	email: string;
@@ -183,12 +134,13 @@ class User {
 class Customer {
 	customerId: string;
 	name: string;
+	line: Line;
 }
 
 class Line {
 	lineId: string;
-	count?: number;
-	user: User;
+	// count?: number;
+	// user: User;
 	// foo: SomeJSON;
 }
 ;
