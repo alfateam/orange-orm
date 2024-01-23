f748ecc29ab0761ff405251f118f0c1afa89e434
Lars-Erik Roald <lars.roald@systor.st>
2024-01-23 15:31:31 +0100

    removed only

--- tests/conflicts.test.js
+++ tests/conflicts.test.js
@@ -412,16 +412,16 @@ describe('insert overwrite with skipOnConflict column', () => {
 	}
 });
 
-describe.only('insert overwrite with optimistic column changed', () => {
-	// test('pg', async () => await verify('pg'));
+describe('insert overwrite with optimistic column changed', () => {
+	test('pg', async () => await verify('pg'));
 	test('oracle', async () => await verify('oracle'));
-	// test('mssql', async () => await verify('mssql'));
-	// if (major === 18)
-	// 	test('mssqlNative', async () => await verify('mssqlNative'));
-	// test('mysql', async () => await verify('mysql'));
-	// test('sqlite', async () => await verify('sqlite'));
-	// test('sap', async () => await verify('sap'));
-	// test('http', async () => await verify('http'));
+	test('mssql', async () => await verify('mssql'));
+	if (major === 18)
+		test('mssqlNative', async () => await verify('mssqlNative'));
+	test('mysql', async () => await verify('mysql'));
+	test('sqlite', async () => await verify('sqlite'));
+	test('sap', async () => await verify('sap'));
+	test('http', async () => await verify('http'));
 
 	async function verify(dbName) {
 		let { db, init } = getDb(dbName);
