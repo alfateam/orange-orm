b0b7246ad2d59682c38f230bf0dad9aca7e3466b
Lars-Erik Roald <lars.roald@systor.st>
2024-02-03 11:56:06 +0000

    comment in tests for offset

--- tests/getMany.test.js
+++ tests/getMany.test.js
@@ -127,15 +127,15 @@ beforeAll(async () => {
 }, 15000);
 
 describe('offset', () => {
-	// test('pg', async () => await verify('pg'));
+	test('pg', async () => await verify('pg'));
 	test('oracle', async () => await verify('oracle'));
-	// test('mssql', async () => await verify('mssql'));
-	// if (major === 18)
-	// 	test('mssqlNative', async () => await verify('mssqlNative'));
-	// test('mysql', async () => await verify('mysql'));
-	// test('sqlite', async () => await verify('sqlite'));
-	// test('sap', async () => await verify('sap'));
-	// test('http', async () => await verify('http'));
+	test('mssql', async () => await verify('mssql'));
+	if (major === 18)
+		test('mssqlNative', async () => await verify('mssqlNative'));
+	test('mysql', async () => await verify('mysql'));
+	test('sqlite', async () => await verify('sqlite'));
+	test('sap', async () => await verify('sap'));
+	test('http', async () => await verify('http'));
 
 	async function verify(dbName) {
 		const { db } = getDb(dbName);
