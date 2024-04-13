a471189f6efd44bb7db86a9204c2db4f2b616539
Lars-Erik Roald <lars.roald@systor.st>
2024-04-13 08:55:17 +0000

    mysql@3.9.4

--- tests/getMany.test.js
+++ tests/getMany.test.js
@@ -468,17 +468,17 @@ describe('getAll orderBy array', () => {
 	}
 });
 
-describe('getMany with column strategy', () => {
+describe.only('getMany with column strategy', () => {
 
-	test('pg', async () => await verify('pg'));
-	test('oracle', async () => await verify('oracle'));
-	test('mssql', async () => await verify('mssql'));
-	if (major === 18)
-		test('mssqlNative', async () => await verify('mssqlNative'));
+	// test('pg', async () => await verify('pg'));
+	// test('oracle', async () => await verify('oracle'));
+	// test('mssql', async () => await verify('mssql'));
+	// if (major === 18)
+	// 	test('mssqlNative', async () => await verify('mssqlNative'));
 	test('mysql', async () => await verify('mysql'));
-	test('sqlite', async () => await verify('sqlite'));
-	test('sap', async () => await verify('sap'));
-	test('http', async () => await verify('http'));
+	// test('sqlite', async () => await verify('sqlite'));
+	// test('sap', async () => await verify('sap'));
+	// test('http', async () => await verify('http'));
 
 	async function verify(dbName) {
 		const { db } = getDb(dbName);
