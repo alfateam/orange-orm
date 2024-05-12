ff2e72fdfef090553a10dc55983371bb7cc98104
Lars-Erik Roald <lars.roald@systor.st>
2024-05-12 20:09:55 +0000

    delete tests

---
+++ tests/delete.test.js
@@ -0,0 +1,242 @@
+import { describe, test, beforeAll, afterAll, expect } from 'vitest';
+import { fileURLToPath } from 'url';
+const map = require('./db');
+import express from 'express';
+import cors from 'cors';
+import { json } from 'body-parser';
+const initPg = require('./initPg');
+const initOracle = require('./initOracle');
+const initMs = require('./initMs');
+const initMysql = require('./initMysql');
+const initSqlite = require('./initSqlite');
+const initSap = require('./initSap');
+
+const versionArray = process.version.replace('v', '').split('.');
+const major = parseInt(versionArray[0]);
+
+const date1 = new Date(2022, 0, 11, 9, 24, 47);
+const date2 = new Date(2021, 0, 11, 12, 22, 45);
+let server = null;
+const port = 3002;
+
+afterAll(async () => {
+	return new Promise((res) => {
+		if (server)
+			server.close(res);
+		else
+			res();
+	});
+});
+
+
+beforeAll(async () => {
+	await insertData('pg');
+	await insertData('oracle');
+	await insertData('mssql');
+	if (major === 18)
+		await insertData('mssqlNative');
+	await insertData('mysql');
+	await insertData('sap');
+	await insertData('sqlite');
+	await insertData('sqlite2');
+	hostExpress();
+
+	async function insertData(dbName) {
+		const { db, init } = getDb(dbName);
+		await init(db);
+
+		const george = await db.customer.insert({
+			name: 'George',
+			balance: 177,
+			isActive: true
+		});
+
+		const john = await db.customer.insert({
+			name: 'Harry',
+			balance: 200,
+			isActive: true
+		});
+
+		await db.order.insert([
+			{
+				orderDate: date1,
+				customer: george,
+				deliveryAddress: {
+					name: 'George',
+					street: 'Node street 1',
+					postalCode: '7059',
+					postalPlace: 'Jakobsli',
+					countryCode: 'NO'
+				},
+				lines: [
+					{ product: 'Bicycle' },
+					{ product: 'Small guitar' }
+				]
+			},
+			{
+				customer: john,
+				orderDate: date2,
+				deliveryAddress: {
+					name: 'Harry Potter',
+					street: '4 Privet Drive, Little Whinging',
+					postalCode: 'GU4',
+					postalPlace: 'Surrey',
+					countryCode: 'UK'
+				},
+				lines: [
+					{ product: 'Magic wand' }
+				]
+			}
+		]);
+	}
+
+	function hostExpress() {
+		const { db } = getDb('sqlite2');
+		let app = express();
+		app.disable('x-powered-by')
+			.use(json({ limit: '100mb' }))
+			.use('/rdb', cors(), db.express());
+		server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+	}
+
+}, 20000);
+
+describe('deleteCascade', () => {
+
+	test('pg', async () => await verify('pg'));
+	test('oracle', async () => await verify('oracle'));
+	test('mssql', async () => await verify('mssql'));
+	if (major === 18)
+		test('mssqlNative', async () => await verify('mssqlNative'));
+	test('mysql', async () => await verify('mysql'));
+	test('sap', async () => await verify('sap'));
+	test('sqlite', async () => await verify('sqlite'));
+
+	async function verify(dbName) {
+
+		const { db } = getDb(dbName);
+
+		let row = await db.order.getOne();
+		const id = row.id;
+		await db.order.deleteCascade({id});
+		let deletedRow = await db.order.getOne({id});
+		expect(deletedRow).toEqual(undefined);
+	}
+});
+describe('deleteCascade http', () => {
+
+	test('http', async () => await verify('http'));
+
+	async function verify(dbName) {
+
+		const { db } = getDb(dbName);
+
+		let row = await db.order.getOne();
+		let message;
+		const id = row.id;
+		try {
+			await db.order.deleteCascade({id});
+		}
+		catch(e) {
+			message = e.message;
+		}
+		expect(message).toEqual('Disallowed operator deleteCascade');
+	}
+});
+
+
+
+const pathSegments = fileURLToPath(import.meta.url).split('/');
+const lastSegment = pathSegments[pathSegments.length - 1];
+const fileNameWithoutExtension = lastSegment.split('.')[0];
+const sqliteName = `demo.${fileNameWithoutExtension}.db`;
+const sqliteName2 = `demo.${fileNameWithoutExtension}2.db`;
+
+const connections = {
+	mssql: {
+		db:
+			map({
+				db: (con) => con.mssql({
+					server: 'mssql',
+					options: {
+						encrypt: false,
+						database: 'master'
+					},
+					authentication: {
+						type: 'default',
+						options: {
+							userName: 'sa',
+							password: 'P@assword123',
+						}
+					}
+				}, { size: 1 })
+			},),
+		init: initMs
+	},
+	mssqlNative:
+	{
+		db: map({ db: (con) => con.mssqlNative('server=mssql;Database=demo;Trusted_Connection=No;Uid=sa;pwd=P@assword123;Driver={ODBC Driver 18 for SQL Server};TrustServerCertificate=yes', { size: 0 }) }),
+		init: initMs
+	},
+	pg: {
+		db: map({ db: con => con.postgres('postgres://postgres:postgres@postgres/postgres', { size: 1 }) }),
+		init: initPg
+	},
+	sqlite: {
+		db: map({ db: (con) => con.sqlite(sqliteName, { size: 1 }) }),
+		init: initSqlite
+	},
+	sqlite2: {
+		db: map({ db: (con) => con.sqlite(sqliteName2, { size: 1 }) }),
+		init: initSqlite
+	},
+	sap: {
+		db: map({ db: (con) => con.sap(`Driver=${__dirname}/libsybdrvodb.so;SERVER=sapase;Port=5000;UID=sa;PWD=sybase;DATABASE=master`, { size: 1 }) }),
+		init: initSap
+	},
+	oracle: {
+		db: map({
+			db: (con) => con.oracle(
+				{
+					user: 'sys',
+					password: 'P@assword123',
+					connectString: 'oracle/XE',
+					privilege: 2
+				}, {size: 1}
+
+			)
+		}),
+		init: initOracle
+	},
+	mysql: {
+		db: map({ db: (con) => con.mysql('mysql://test:test@mysql/test', { size: 1 }) }),
+		init: initMysql
+	},
+	http: {
+		db: map.http(`http://localhost:${port}/rdb`),
+	}
+
+};
+
+function getDb(name) {
+	if (name === 'mssql')
+		return connections.mssql;
+	else if (name === 'mssqlNative')
+		return connections.mssqlNative;
+	else if (name === 'pg')
+		return connections.pg;
+	else if (name === 'sqlite')
+		return connections.sqlite;
+	else if (name === 'sqlite2')
+		return connections.sqlite2;
+	else if (name === 'sap')
+		return connections.sap;
+	else if (name === 'oracle')
+		return connections.oracle;
+	else if (name === 'mysql')
+		return connections.mysql;
+	else if (name === 'http')
+		return connections.http;
+	else
+		throw new Error('unknown');
+}
