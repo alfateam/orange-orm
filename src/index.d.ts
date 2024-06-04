27d619a23e44539be4a553025f978a5e6eb22baa
Lars-Erik Roald <lars-erik.roald@systor.st>
2024-06-04 08:24:53 +0200

    split types

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,7 +2,8 @@ import type { Options } from './ajv';
 import type { RequestHandler } from 'express';
 import type { ConnectionConfiguration } from 'tedious';
 import type { PoolAttributes } from 'oracledb';
-import type { AllowedDbMap, DbMapper, MappedDbDef } from './map';
+import type { DbMapper, MappedDbDef } from './map';
+import type { AllowedDbMap } from './types';
 
 declare function r(config: r.Config): unknown;
 
@@ -21,8 +22,8 @@ declare namespace r {
     function on(type: 'query', cb: (e: QueryEvent) => void): void;
     function off(type: 'query', cb: (e: QueryEvent) => void): void;
     function map<V extends AllowedDbMap<V>>(
-		fn: (mapper: DbMapper<{}>) => V
-	): MappedDbDef<V>;
+        fn: (mapper: DbMapper<{}>) => V
+    ): MappedDbDef<V>;
     function createPatch(original: any[], modified: any[]): JsonPatch;
     function createPatch(original: any, modified: any): JsonPatch;
 
