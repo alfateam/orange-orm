d996b6ffee2ab2c59f327e4b7b534ccbe87ea4dd
Lars-Erik Roald <lars-erik.roald@systor.st>
2024-11-19 08:16:06 +0100

    Add support for date-only ISO strings closes #116

--- src/table/column/date/tryParseISO.js
+++ src/table/column/date/tryParseISO.js
@@ -1,14 +1,12 @@
-var pattern = /(\d{4}-[01]\d-[0-3]\d(T| )[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\d(T| )[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\d(T| )[0-2]\d:[0-5]\d)/;
+var patternWithTime = /(\d{4}-[01]\d-[0-3]\d(T| )[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\d(T| )[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\d(T| )[0-2]\d:[0-5]\d)/;
+var patternDateOnly = /^\d{4}-[01]\d-[0-3]\d$/;
 
 function tryParseISO(iso) {
-	if (pattern.test(iso))
+	if (patternWithTime.test(iso) || patternDateOnly.test(iso)) {
 		return iso;
-	else
-		throw new TypeError(iso +  ' is not a valid Date');
-
-
-
-
+	} else {
+		throw new TypeError(iso + ' is not a valid Date');
+	}
 }
 
 module.exports = tryParseISO;
