16621234157fedbb2fb4f16e45b756b93eba3ed0
Lars-Erik Roald <lars.roald@systor.st>
2026-02-14 08:22:17 +0000

    colum-to-column filters

--- src/table/column/string/endsWithCore.js
+++ src/table/column/string/endsWithCore.js
@@ -1,13 +1,17 @@
 const quote = require('../../quote');
 var newBoolean = require('../newBoolean');
 var nullOperator = ' is ';
+var encodeFilterArg = require('../encodeFilterArg');
+var newLikeColumnArg = require('./newLikeColumnArg');
 
 function endsWithCore(context, operator, column,arg,alias) {
 	alias = quote(context, alias);
 	operator = ' ' + operator + ' ';
-	var encoded = column.encode(context, arg);
+	var encoded = encodeFilterArg(context, column, arg);
 	if (encoded.sql() == 'null')
 		operator = nullOperator;
+	else if (arg && typeof arg._toFilterArg === 'function')
+		encoded = newLikeColumnArg(context, column, encoded, '%', null);
 	else
 		encoded = column.encode(context, '%' + arg);
 	var firstPart = alias + '.' + quote(context, column._dbName) + operator;
--- src/table/column/string/startsWithCore.js
+++ src/table/column/string/startsWithCore.js
@@ -1,12 +1,16 @@
 var newBoolean = require('../newBoolean');
 var nullOperator = ' is ';
 var quote = require('../../quote');
+var encodeFilterArg = require('../encodeFilterArg');
+var newLikeColumnArg = require('./newLikeColumnArg');
 
 function startsWithCore(context, operator, column,arg,alias) {
 	operator = ' ' + operator + ' ';
-	var encoded = column.encode(context, arg);
+	var encoded = encodeFilterArg(context, column, arg);
 	if (encoded.sql() == 'null')
 		operator = nullOperator;
+	else if (arg && typeof arg._toFilterArg === 'function')
+		encoded = newLikeColumnArg(context, column, encoded, null, '%');
 	else
 		encoded = column.encode(context, arg + '%');
 	var firstPart = quote(context, alias) + '.' + quote(context, column._dbName) + operator;
